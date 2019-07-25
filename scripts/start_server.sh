92473af104df1681f37184026c5a0b4524f695de
Gulsanara <zubaidabizly@yahoo.com>
2019-07-25 17:02:53 +0600

    New Addition

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# sudo chmod 755 /var/www/server.js # optional
+# sudo chmod 755 /var/www/app.js # optional
 # this will restart app/server on instance reboot
-crontab -l | { cat; echo "@reboot pm2 start /var/www/server.js -i 0 --name \"node-app\""; } | crontab -
+crontab -l | { cat; echo "@reboot pm2 start /var/www/app.js -i 0 --name \"node-app\""; } | crontab -
 sudo pm2 stop node-app
 # actually start the server
-sudo pm2 start /var/www/server.js -i 0 --name "node-app"
+sudo pm2 start /var/www/app.js -i 0 --name "node-app"
 
